c348c6f5f864d77f32d0a9a9185760f4cc0b7223
volkan-sahin <volkanesvo@gmail.com>
2022-06-22 19:20:18 +0300

    add phonebook project

--- python/hands-on/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
+++ python/hands-on/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
@@ -6,7 +6,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'rds-handson.czb71ppc8yio.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_HOST'] = 'rdscim.czb71ppc8yio.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway'
@@ -35,7 +35,8 @@ VALUES
 	("Ethan", "ethan@micrasoft.com"),
 	("mostafa", "mostafa@facebook.com"),
     ("sait", "sait@tesla.com"),
-    ("busra","busra@google");
+    ("busra","busra@google"),
+    ("busra10","busra@as");
 """
 cursor.execute(drop_table)
 cursor.execute(users_table)
@@ -107,5 +108,5 @@ def add_email():
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__ == '__main__':
-   app.run(debug=True)
-   # app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=80)
